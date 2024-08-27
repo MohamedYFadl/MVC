238b7430a02bac2d5ec1d5e27df92fdbab8b59d5
MomoYasser0 <myasoo28@gmail.com>
2024-08-27 22:54:44 +0300

    What is happening Before Routing

--- Program.cs
+++ Program.cs
@@ -10,32 +10,66 @@ namespace MVC
             //app.MapGet("/", () => "Hello World!");
 
             app.UseRouting();
-            app.UseEndpoints(endPoint => {
+            //app.Use(async (context, next) => {
+            //    Endpoint endpoint = context.GetEndpoint();
+            //    if (endpoint != null)
+            //        await context.Response.WriteAsync(endpoint.DisplayName);
+
+            //    await next(context);
+
+            //});
+            //app.UseEndpoints(endPoint =>
+            //{
+
+            //    endPoint.Map("/", async (context) =>
+            //    {
+            //        await context.Response.WriteAsync("Your are in Default Page ");
+            //    });
+
+            //    endPoint.Map("/Home", async (context) =>
+            //    {
+            //        await context.Response.WriteAsync("Your are in Home Page ");
+            //    });
+
+            //    endPoint.MapGet("/Products", async (context) =>
+            //    {
+            //        await context.Response.WriteAsync("Your are in Products Page ");
+            //    });
+            //    endPoint.MapPost("/Products", async (context) =>
+            //    {
+            //        await context.Response.WriteAsync("Products Has been Created!");
+            //    });
+            //});
+            app.UseEndpoints(endPoint =>
+            {
 
                 endPoint.Map("/", async (context) =>
                 {
                     await context.Response.WriteAsync("Your are in Default Page ");
                 });
 
-                endPoint.Map("/Home", async (context) =>
-                {
-                    await context.Response.WriteAsync("Your are in Home Page ");
-                });
 
                 endPoint.MapGet("/Products", async (context) =>
                 {
-                    await context.Response.WriteAsync("Your are in Products Page ");
-                });
-                endPoint.MapPost("/Products", async (context) =>
-                {
                     await context.Response.WriteAsync("Products Has been Created!");
+                });
+                endPoint.MapGet("/Products/Id={id}", async (context) =>
+                {
+                    var id = Convert.ToInt32(context.Request.RouteValues["id"]);
+                    await context.Response.WriteAsync($"Your getting Product with {id}");
+                });
+                endPoint.MapGet("/Books/Author/{authorName}/{bookId}", async (context) =>
+                {
+                    var bookId = Convert.ToInt32(context.Request.RouteValues["bookId"]);
+                    var authorName = Convert.ToString(context.Request.RouteValues["authorName"]);
+                    await context.Response.WriteAsync($"Your getting Product with {bookId} and Author : {authorName}");
                 });
             });
 
-
-            app.Run(async (HttpContext context)=> {
+            app.Run(async (HttpContext context) =>
+            {
                 await context.Response.WriteAsync("The Page is NOt Found");
-            
+
             });
             app.Run();
         }
